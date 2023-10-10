c27cfc9cbc85bccfffc63f578a57888b89f712d2
Emma <ecardosa@cendrassos.net>
2023-10-10 19:43:46 +0200

    test

--- index.php
+++ index.php
@@ -1,4 +1,37 @@
-<?php
-$var = 1;
-$home = 1;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link  type="text/css" rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Apartaments Figuerencs</title>
+</head>
+<body>
+    <header>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                    <img src="fotos/casa.png">
+                </a>
+                <p class="hover-text">Página principal</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img id="star" src="fotos/estrella.png">
+                </a>
+                <p class="hover-text">Reserves</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">
+                    <img src="fotos/user.png">
+                </a>
+                <p class="hover-text">Perfil</p>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Log out</a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
