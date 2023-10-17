d54e1f35bdc18e63f61592916f099ca537b1a935
Emma <ecardosa@cendrassos.net>
2023-10-17 19:43:32 +0200

    Development

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
     error_reporting(E_ERROR | E_PARSE);
     include "src/controls/index.php";
-<<<<<<< HEAD
+
     include "src/controls/login.php";
     include "src/controls/registration.php";
 
-=======
+
     error_reporting(E_ERROR | E_PARSE);
->>>>>>> 7d25a4a (Semana 2)
+
     $r = $_REQUEST["r"];
 
     if($r==""){
--- src/views/mainmenu.php
+++ src/views/mainmenu.php
@@ -31,9 +31,7 @@
                 </li>
                
             </ul>
-            <button class="login">
-                Log in
-            </button>
+        
         </header>
         
     </body>
