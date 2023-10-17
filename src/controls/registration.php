a9d7d8c1986cd0362ffa01f59c489522a21442f7
Anton Stamov <stamovanton233@gmail.com>
2023-10-17 17:27:52 +0200

    Database connection

--- src/controls/registration.php
+++ src/controls/registration.php
@@ -20,9 +20,11 @@
         if (isset($_POST["submit"])){
             if(isset($_POST["login"])&&isset($_POST["name"])&&isset($_POST["surename"])&&isset($_POST["email"])&&isset($_POST["password"])&&isset($_POST["phone"])&&isset($_POST["address"])&&isset($_POST["address2"])&&isset($_POST["city"])&&isset($_POST["zip"])){
                 $sql = "INSERT INTO Usuario (Nombre,Apellidos,Telefono,CorreoElectronico,Rol,Direccion,Direccion2,Ciudad,CodigoPostal,login,password)
-                                            (".$_POST['name'].",".$_POST['surename'].",".$_POST['phone'].",".$_POST['email'].",'cliente',".$_POST['address'].",".$_POST['address2'].",".$_POST['city'].",".$_POST['zip'].",".$_POST['login'].",".$_POST['passowrd'].");";
+                (".$_POST['name'].",".$_POST['surename'].",".$_POST['phone'].",".$_POST['email'].",cliente,".$_POST['address'].",".$_POST['address2'].",".$_POST['city'].",".$_POST['zip'].",".$_POST['login'].",".$_POST['passowrd'].");";
+                echo $sql;
                 $stmt = $conn->prepare($sql);
                 $stmt->execute();
+
 
                
             }
